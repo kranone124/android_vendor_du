0e2d1cd29b7351489a04b02bc023aae05047ea48
kranone124 <kranone1234@gmail.com>
2018-03-09 17:19:03 +0100

    Update caf-vendorsetup.sh

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -3,3 +3,4 @@ add_lunch_combo du_dumpling-userdebug
 add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_potter-userdebug
 add_lunch_combo du_tenderloin-userdebug
+add_lunch_combo du_axon7-userdebug
